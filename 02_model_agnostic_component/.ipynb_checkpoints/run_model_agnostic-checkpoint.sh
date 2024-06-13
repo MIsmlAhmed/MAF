78be1eaac4fb4d6f54f95c15509bd1778f164dd4
Mohamed Ahmed <mia725@gra-login2.graham.sharcnet>
2024-06-13 15:52:53 -0400

    fix agnos and spec for HYPE

--- 02_model_agnostic_component/.ipynb_checkpoints/run_model_agnostic-checkpoint.sh
+++ 02_model_agnostic_component/.ipynb_checkpoints/run_model_agnostic-checkpoint.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # load the needed modules
 module restore scimods
+
 # load python environments (that contains all needed packages)
 source $HOME/virtual-envs/scienv/bin/activate
+
 #run the model agnostic framework
 ./model-agnostic.sh model-agnostic.json
